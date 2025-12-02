e614276f1e1e5161ce669f36f739cb9ce37a7317
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2025-12-02 10:44:16 +0100

    rework: made notes spawn on start instead of progressively in the FixedUpdate

--- Assets/Scripts/LevelLauncher.cs
+++ Assets/Scripts/LevelLauncher.cs
@@ -9,45 +9,32 @@ public class LevelLauncher : MonoBehaviour
   public List<Transform> columns = new();
   public List<Transform> notes = new();
 
+	readonly float startPauseTime = 3f;
   readonly float speed = 5f;
-
-  float elapsedTime;
-  int currentNoteIndex;
 
   void Start()
   {
-    elapsedTime = 0;
-    currentNoteIndex = 0;
-
     foreach (Transform child in transform)
     {
       columns.Add(child);
+    }
+
+		foreach (NoteRow noteRow in level.noteRows)
+    {
+			float posZ = (startPauseTime + noteRow.time) * speed;
+
+      foreach (Note note in noteRow.notes)
+        {
+          GameObject newNote = Instantiate(tapNote, new Vector3(0f, 0f, posZ), Quaternion.Euler(new Vector3(90f, 0f, 0f)));
+          newNote.transform.parent = columns[note.column];
+          newNote.transform.localPosition = new Vector3(0f, 0f, newNote.transform.localPosition.z);
+          notes.Add(newNote.transform);
+        }
     }
   }
 
   void FixedUpdate()
   {
-    elapsedTime += Time.deltaTime;
-
-    if (currentNoteIndex < level.noteRows.Count)
-    {
-      while (level.noteRows[currentNoteIndex].time <= elapsedTime)
-      {
-        NoteRow noteRow = level.noteRows[currentNoteIndex];
-        foreach (Note note in noteRow.notes)
-        {
-          GameObject newNote = Instantiate(tapNote, Vector3.zero, Quaternion.Euler(new Vector3(90f, 0f, 0f)));
-          newNote.transform.parent = columns[note.column];
-          newNote.transform.localPosition = new Vector3(0f, 0f, 20f);
-          notes.Add(newNote.transform);
-        }
-
-        currentNoteIndex++;
-
-        if (currentNoteIndex >= level.noteRows.Count) break;
-      }
-    }
-
     float step = speed * Time.deltaTime;
     Vector3 movement = step * new Vector3(0f, 0f, -1f);
 
